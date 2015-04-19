2f3f7b2f1ae21e71d24525e2384b8b2aeb52352b
luis rodriguez <luis.rodriguez@cru.org>
2015-04-19 19:35:50 -0400

    add install for packages

--- recipes/_base_aem_setup.rb
+++ recipes/_base_aem_setup.rb
@@ -17,7 +17,12 @@
 # limitations under the License.
 
 # We need these for the jcr_node provider
-
+ %w{libcurl4-openssl-dev libcurl4-gnutls-dev maven ruby}.each do |pkg|
+    package pkg do
+      action :install
+    end
+  end  
+  
 gem_package "bundler" do
   action :install
   ignore_failure true
